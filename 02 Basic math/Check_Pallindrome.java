e51e30333a6bce86f26d3d29d621b842576387a7
saurabh007007 <loverboybhol143@gmail.com>
2024-08-19 19:37:40 +0530

    added pallindrome no check

--- 02 Basic math/Check_Pallindrome.java
+++ 02 Basic math/Check_Pallindrome.java
@@ -1,5 +1,24 @@
-package 02 Basic math;
+import java.util.*;;
 
 public class Check_Pallindrome {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no to check pallindroe");
+        int n = sc.nextInt();
+        if (Pallindrome((n))) {
+            System.out.println("Pallindeome hai bhai");
+        } else {
+            System.out.println("Pallindrome nhi hai bhai");
+        }
+    }
+
+    public static boolean Pallindrome(int n) {
+        int a = 0;
+        int b = n;
+        while (n != 0) {
+            a = a * 10 + n % 10;
+            n = n / 10;
+        }
+        return a == b;
+    }
 }
